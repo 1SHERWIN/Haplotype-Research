90bdba6928446e9dca91ad096de16f478c3941bb
1SHERWIN <31481757+1SHERWIN@users.noreply.github.com>
2019-12-22 15:53:59 -0600

    Update compare3HA.pl

--- compare3HA.pl
+++ compare3HA.pl
@@ -1,8 +1,8 @@
-# 2019 November
+# 2019 December
 # Author: Sherwin Massoudian
 # This script compares the SIHA results of 3 packages 
 
-# perl ./compare2HA.pl peath.4col.txt hapcut.4col.txt whatshap.4col.txt 1 results.txt
+# perl ./compare3HA.pl peath.4col.txt hapcut.4col.txt whatshap.4col.txt 1 results.txt
 
  
 # 1st argument should have 4 columns: position hap1 hap2 BlockID
@@ -26,8 +26,8 @@
 # 76294   0       1       2
 # 77582   0       1       3
 
-# 4th argument decides to use 1st or 2nd package block ID
-# 1 or 2
+# 4th argument decides which block ID is used as a key
+# 1-3
 
 # 5th argument will be the output
 
@@ -94,34 +94,34 @@ foreach(sort { $a <=> $b } @intersect) {
 
 
 
-# output the intersects of two results
-@intersect = grep { exists $snpsB{$_} } keys %snpsA;
-foreach(sort { $a <=> $b } @intersect) {
-	print $outA "$snpsA{$_}\n";
-	delete $snpsA{$_};
-	print $outB "$snpsB{$_}\n";
-	delete $snpsB{$_};
-	print $outC "-\t-\t-\t10000\n";
-}
-@intersect  = ();
-@intersect = grep { exists $snpsC{$_} } keys %snpsA;
-foreach(sort { $a <=> $b } @intersect) {
-	print $outA "$snpsA{$_}\n";
-	delete $snpsA{$_};
-	print $outC "$snpsC{$_}\n";
-	delete $snpsC{$_};
-	print $outB "-\t-\t-\t10000\n";
-}
-@intersect  = ();
-@intersect = grep { exists $snpsC{$_} } keys %snpsB;
-foreach(sort { $a <=> $b } @intersect) {
-	print $outB "$snpsB{$_}\n";
-	delete $snpsB{$_};
-	print $outC "$snpsC{$_}\n";
-	delete $snpsC{$_};
-	print $outA "-\t-\t-\t10000\n";
-}
-@intersect  = ();
+# # output the intersects of two results
+# @intersect = grep { exists $snpsB{$_} } keys %snpsA;
+# foreach(sort { $a <=> $b } @intersect) {
+	# print $outA "$snpsA{$_}\n";
+	# delete $snpsA{$_};
+	# print $outB "$snpsB{$_}\n";
+	# delete $snpsB{$_};
+	# print $outC "-\t-\t-\t10000\n";
+# }
+# @intersect  = ();
+# @intersect = grep { exists $snpsC{$_} } keys %snpsA;
+# foreach(sort { $a <=> $b } @intersect) {
+	# print $outA "$snpsA{$_}\n";
+	# delete $snpsA{$_};
+	# print $outC "$snpsC{$_}\n";
+	# delete $snpsC{$_};
+	# print $outB "-\t-\t-\t10000\n";
+# }
+# @intersect  = ();
+# @intersect = grep { exists $snpsC{$_} } keys %snpsB;
+# foreach(sort { $a <=> $b } @intersect) {
+	# print $outB "$snpsB{$_}\n";
+	# delete $snpsB{$_};
+	# print $outC "$snpsC{$_}\n";
+	# delete $snpsC{$_};
+	# print $outA "-\t-\t-\t10000\n";
+# }
+# @intersect  = ();
 
 
 
@@ -130,17 +130,20 @@ foreach(sort { $a <=> $b } @intersect) {
 foreach(sort { $a <=> $b } keys %snpsA) {
 	print $outA "$snpsA{$_}\n";
 	print $outB "-\t-\t-\t10000\n";
+	print $outC "-\t-\t-\t10000\n";
 	delete $snpsA{$_};
 }
 foreach(sort { $a <=> $b } keys %snpsB) {
 	print $outB "$snpsB{$_}\n";
 	print $outA "-\t-\t-\t10000\n";
+	print $outC "-\t-\t-\t10000\n";
 	delete $snpsB{$_};
 }
 foreach(sort { $a <=> $b } keys %snpsC) {
-	print $outB "$snpsC{$_}\n";
+	print $outC "$snpsC{$_}\n";
 	print $outA "-\t-\t-\t10000\n";
-	delete $snpsB{$_};
+	print $outB "-\t-\t-\t10000\n";
+	delete $snpsC{$_};
 }
 
 
@@ -150,41 +153,51 @@ system("paste package1.txt package2.txt > 8col.txt");
 system("paste 8col.txt package3.txt > 12col.txt");
 
 # Sort file on key
-my $key = $ARGV[2] * 4;
+my $key = $ARGV[3] * 4;
 system("sort -n -k $key 12col.txt > sorted12col.txt");
 
 # TODO: calculate agreement
-# open(IN1, "<sorted12col.txt") or die "Error opening sorted12col.txt\n";
-# open my $out, '>', $ARGV[4] or die $!;
+open(IN1, "<sorted12col.txt") or die "Error opening sorted12col.txt\n";
+open my $out, '>', $ARGV[4] or die $!;
 
 
-# # index of each haplotype
-# my $A1 = 1;
-# my $A2 = 2;
-# my $B1 = 5;
-# my $B2 = 6;
+# index of each haplotype
+my $A1 = 1;
+my $A2 = 2;
+my $B1 = 5;
+my $B2 = 6;
+my $C1 = 9;
+my $C2 = 10;
 
 
-# # String to record Haplotype blocks
-# my $aHap1 = "";
-# my $aHap2 = "";
-# my $bHap1 = "";
-# my $bHap2 = "";
-# my $snvinblock;
-# my $snvCountA;
-# my $snvCountB;
-# my $posMatch = "";
-# my $hapMatch = "";
-# my $block = 0;
-# my $totalMatchedBlock = 0;
-# my $totalMatchedSNV = 0;
-# $key--;
+# String to record Haplotype blocks
+my $aHap1 = "";
+my $aHap2 = "";
+my $bHap1 = "";
+my $bHap2 = "";
+my $cHap1 = "";
+my $cHap2 = "";
+my $posMatch = "-";
+my $hapMatch = "-";
+my $snvinblock = 0;
+my $snvCountA = 0;
+my $snvCountB = 0;
+my $snvCountC = 0;
+my $block = 0;
+my $totalMatchedBlock = 0;
+my $totalMatchedSNV = 0;
+$key--;
 
 
-# sub getAgreement(){
-	# $snvCountA = length($aHap1);
-	# $snvCountB = length($bHap1);
-	# $posMatch = $snvCountA == $snvCountB ? "match" : "-";
+sub getPosAgreement(){
+	$snvCountA = length($aHap1);
+	$snvCountB = length($bHap1);
+	$snvCountC = length($cHap1);
+	if ($snvCountA == $snvCountB && $snvCountB == $snvCountC) {
+		$posMatch = "match";
+	}
+}
+sub getHapAgreement(){
 	# if ($aHap1 eq $bHap1 and $aHap2 eq $bHap2) {
 		# $hapMatch = "match";
 		# $totalMatchedSNV += $snvinblock;
@@ -198,56 +211,64 @@ system("sort -n -k $key 12col.txt > sorted12col.txt");
 	# else {
 		# $hapMatch = "-";
 	# }
-# }
-# print $out "Block\tCountA\tCountB\tSNVs\tHaplotype\n";
-# sub printAgreement(){
-	# print $out "$block\t$snvCountA\t$snvCountB\t$posMatch\t$hapMatch\n";
+}
+print $out "Block\tCountA\tCountB\tCountC\tSNVs\tHaplotype\n";
+sub printAgreement(){
+	print $out "$block\t$snvCountA\t$snvCountB\t$snvCountC\t$posMatch\t$hapMatch\n";
+}
+sub resetCounts(){
+	$aHap1 = "";
+	$aHap2 = "";
+	$bHap1 = "";
+	$bHap2 = "";
+	$cHap1 = "";
+	$cHap2 = "";
+	$posMatch = "-";
+	$hapMatch = "-";
+	$snvinblock = 0;	
+}
+while (!eof(IN1)) {
+	my $line = <IN1>;
+	chomp $line;
+	
+	my @position = split(/	/, $line);
+	if ($block == 0) {
+		$block = $position[$key];
+	}
+	
+	# Count the SNPs in each block
+	$snvinblock++;
+	
+	# Print package agreement at the end of a block
+	if ($block != $position[$key]) {
+		getPosAgreement();
+		getHapAgreement()
+		printAgreement();
+		resetCounts();
+		$block = $position[$key];
+	}
+	
+	
+	# Add genotype in current line
+	$aHap1 .= $position[$A1];
+	$aHap2 .= $position[$A2];
+	$bHap1 .= $position[$B1];
+	$bHap2 .= $position[$B2];
+	$cHap1 .= $position[$C1];
+	$cHap2 .= $position[$C2];
+	
+}
 
-# }
-# while (!eof(IN1)) {
-	# my $line = <IN1>;
-	# chomp $line;
-	
-	# my @position = split(/	/, $line);
-	# if ($block == 0) {
-		# $block = $position[$key];
-	# }
-	
-	# # Count the SNPs in each block
-	# $snvinblock++;
-	
-	# # Print package agreement at the end of a block
-	# if ($block != $position[$key]) {
-		# getAgreement();
-		# printAgreement();
-		# $block = $position[$key];
-		# $aHap1 = "";
-		# $aHap2 = "";
-		# $bHap1 = "";
-		# $bHap2 = "";
-		# $hapMatch = "-";
-		# $snvinblock = 0;
-	# }
-	
-	
-	# # Add genotype in current line
-	# $aHap1 .= $position[$A1];
-	# $aHap2 .= $position[$A2];
-	# if ($bHap1 ne '-'){
-		# $bHap1 .= $position[$B1];
-		# $bHap2 .= $position[$B2];
-	# }
-# }
+# Print the last block
+getPosAgreement();
+getHapAgreement()
+printAgreement();
 
-# # Print the last block
-# getAgreement();
-# printAgreement();
-
-# close $out;
+close $out;
 
 # Display runtime
-my $runTime = $endTime - $start;
 my $endTime = time();
+my $runTime = $endTime - $start;
 print "Job took $runTime seconds \n";
 
 # Print agreement
